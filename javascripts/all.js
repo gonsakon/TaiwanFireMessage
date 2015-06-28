ed04f123e383cddb661ba1915dd0974c7284a1b9
liao <gonsakon@gmail.com>
2015-06-28 15:14:16 +0800

    update total people

--- javascripts/all.js
+++ javascripts/all.js
@@ -4,10 +4,32 @@ app.controller('PeopleCtrl', function($scope,$http) {
   $scope.keyword = '';
   $scope.data = {};
   $scope.UpdateTime = '';
+  $scope.data.ICUTotal=0;
+  $scope.data.OutTotal = 0;
+  $scope.data.GenealTotal = 0;
+  $scope.data.ObservedTotal = 0;
   $http.get('https://gist.githubusercontent.com/tony1223/098e45623c73274f7ae3/raw').
   success(function(data, status, headers, config) {
     $scope.data.peoples= data.data;
     $scope.UpdateTime= data.lastmodify;
+    var Total = $scope.data.peoples.length;
+    console.log(Total);
+    for(i=0;Total>i;i++){
+      if($scope.data.peoples[i]['即時動向'] == '加護病房'){
+        $scope.data.ICUTotal += 1;
+      }
+      if($scope.data.peoples[i]['即時動向'] == '出院'){
+        $scope.data.OutTotal += 1;
+      }
+      if($scope.data.peoples[i]['即時動向'] == '一般病房'){
+        $scope.data.GenealTotal += 1;
+      }
+      if($scope.data.peoples[i]['即時動向'] == '留觀'){
+        $scope.data.ObservedTotal += 1;
+      }
+
+
+    }
   });
   $scope.nameSearch = function() {
    if ($scope.keyword.length > 1)
