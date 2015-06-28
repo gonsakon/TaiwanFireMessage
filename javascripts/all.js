ae43a26ec046b0c82304dc66fbf2dec7413f96d4
liao <gonsakon@gmail.com>
2015-06-28 23:30:04 +0800

    Name Search secret none

--- javascripts/all.js
+++ javascripts/all.js
@@ -35,8 +35,6 @@ app.controller('PeopleCtrl', function($scope,$http) {
   });
   $scope.nameSearch = function() {
    if ($scope.keyword.length > 1)
-     return $scope.keyword[0] + '○' + $scope.keyword.substr(2, $scope.keyword.length-2);
-   else
      return $scope.keyword;
   };
 });
